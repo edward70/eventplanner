fe91da73f0ad020ff54c45546f7951071b42f44e
Edward <76045667+edward70@users.noreply.github.com>
2022-09-05 13:02:52 +1000

    more comments

--- app.py
+++ app.py
@@ -15,16 +15,17 @@ app.jinja_env.filters['nl2br'] = nl2br # enable newline to br converter plugin
 # hardcoded constants
 auth = {"username": "password"} # username -> password
 names = {"username": "John Doe"} # username -> real name
-approval_order = ["username"]
+# approval order removed since multiuser is outside of scope
+#approval_order = ["username"]
 # school calendar url
 schoolcalendar = "https://docs.google.com/document/d/1IhUHiIH9ctDlDJ2-_vLI42qQm-MGW5-Yxxv0xx_8bQo/edit?usp=sharing"
 # school activity calendar url
 activitycalendar = "https://docs.google.com/document/d/e/2PACX-1vT12_PabQ-wKDFeGW26U1fAT9KhByozRazm7CJcLqHFqfB6cpGpRhHCmOWmIAZOSWpPbIvtqyNqvo8J/pub?embedded=true"
-app.secret_key = "4Fa!R6w1@wkbMUSHO47r7#zmn"
+app.secret_key = "4Fa!R6w1@wkbMUSHO47r7#zmn" # key to encrypt user credentials in browser cookies
 
-# disable thread checking since sqlite3 is thread safe, enable autocommit for faster writes
+# disable thread checking since sqlite3 is thread safe, enable autocommit for faster writes -> database file for sqlite is data.db
 con = sqlite3.connect('data.db', check_same_thread=False, isolation_level=None)
-cur = con.cursor()
+cur = con.cursor() # cursor for database access
 cur.execute('PRAGMA journal_mode=WAL') # enable Write Ahead Logging for super speed
 cur.execute('PRAGMA synchronous=NORMAL') # reduces syncing without corruption risk for WAL mode
 cur.execute('PRAGMA auto_vacuum=FULL') # reduce db file size
@@ -37,22 +38,22 @@ db_signature = '''(name text, organisers text, mainstudentname text,
                 financial text, logistical text, materials text, risks text,
                 requestdetails text, cashtinbool text, floatbool text,
                 cashsupervise text, organisation text, paymentdetails text,
-                eventhash text, approval text)'''
-cur.execute('CREATE TABLE IF NOT EXISTS events {}'.format(db_signature))
-atexit.register(lambda: con.close())
+                eventhash text, approval text)''' # signature for event table in sqlite
+cur.execute('CREATE TABLE IF NOT EXISTS events {}'.format(db_signature)) # create table if it doesn't exist
+atexit.register(lambda: con.close()) # autoclose database
 
-currentYear = datetime.datetime.now().year
+currentYear = datetime.datetime.now().year # get current year
 
-def parse_calendar(filename):
-    wordDoc = Document(filename)
-    lastNum = 0
-    currentMonth = 1
-    datesTaken = []
-    conflicts = []
-    for table in wordDoc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                cell_data = list(filter(lambda x: x, cell.text.split()))
+def parse_calendar(filename): # function to parse calendar files
+    wordDoc = Document(filename) # open document
+    lastNum = 0 # last day number checked (0 for none checked)
+    currentMonth = 1 # start at Jan
+    datesTaken = [] # list to store dates taken
+    conflicts = [] # list to store info for date conflict gui
+    for table in wordDoc.tables: # loop through tables in document
+        for row in table.rows: # loop through table rows
+            for cell in row.cells: # loop through each cell
+                cell_data = list(filter(lambda x: x, cell.text.split())) # get a list of words in the cell
                 
                 if len(cell_data) > 0 and cell_data[0].isdigit(): # data validation: check theres a day number
                     if len(cell_data) > 2: # data validation: check that the event contains text beyond two tokens
@@ -62,27 +63,27 @@ def parse_calendar(filename):
                             currentMonth = currentMonth + 1 # increment month
                         
                         datesTaken.append(datetime.date(currentYear, currentMonth, day).isoformat()) # add date to list
-                        conflicts.append(cell.text[len(cell_data[0])+1:])
-                        lastNum = day
-    return dict(zip(datesTaken, conflicts))
+                        conflicts.append(cell.text[len(cell_data[0])+1:]) # add cell text to conflict list
+                        lastNum = day # change last day to processed day
+    return dict(zip(datesTaken, conflicts)) # convert to dictionary and return
     
-calendar = parse_calendar('calendar.docx')
-lunchtime_calendar = parse_calendar('lunchtimecalendar.docx')
+calendar = parse_calendar('calendar.docx') # parse school calendar
+lunchtime_calendar = parse_calendar('lunchtimecalendar.docx') # parse lunch calendar
 
 # decorator to check authorization for privileged operations
 def privileged(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if auth.get(session.get('user')):
-            return func(*args, **kwargs)
-        else:
-            return render_template('login.html')
-    return wrapper
+    @wraps(func) # wrap function copying all special methods and attributes such as __doc__ (necessary for flask compatibility)
+    def wrapper(*args, **kwargs): # wrap arguments and keyword arguments in a function
+        if auth.get(session.get('user')): # check if session exists
+            return func(*args, **kwargs) # allow privileged access
+        else: # not logged in
+            return render_template('login.html') # redirect to login page
+    return wrapper # return wrapped function
     
 # context manager to handle sql transactions
 # https://charlesleifer.com/blog/going-fast-with-sqlite-and-python/
-@contextmanager
-def transaction(conn):
+@contextmanager # use this function in with statements for RAII style behaviour
+def transaction(conn): # transaction handling function
     # We must issue a "BEGIN" explicitly when running in auto-commit mode.
     conn.execute('BEGIN')
     try:
@@ -92,17 +93,17 @@ def transaction(conn):
         conn.rollback()  # Roll back all changes if an exception occurs.
         raise
     else:
-        conn.commit()
+        conn.commit() # commit changes
         
 # https://stackoverflow.com/questions/41129921/validate-an-iso-8601-datetime-string-in-python
-def datetime_valid(dt_str):
-    try:
-        datetime.fromisoformat(dt_str)
+def datetime_valid(dt_str): # function to check if date is valid
+    try: # handle error
+        datetime.fromisoformat(dt_str) # attempt string to date conversion
     except:
-        return False
-    return True
+        return False # if there's an error, it's not a valid date
+    return True # date is valid
 
-def handle_event(request):
+def handle_event(request): # data spec event handling logic function
     lst = ["name", "organisers", "mainstudentname",
            "mainstudentemail", "teacher", "summary",
            "date", "time", "venue", "whosetup",
@@ -111,14 +112,14 @@ def handle_event(request):
            "furniturebool", "furniture", "assistancebool",
            "financial", "logistical", "materials", "risks",
            "requestdetails", "cashtinbool", "floatbool",
-           "cashsupervise", "organisation", "paymentdetails"]
-    data = [request.form.get(i,"").trim() for i in lst]
-    return data
+           "cashsupervise", "organisation", "paymentdetails"] # data attributes from html form
+    data = [request.form.get(i,"").trim() for i in lst] # put data in a list
+    return data # return the data
     
-def validate_event(data):
+def validate_event(data): # event validation logic function
     pattern = re.compile('((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))') # time regex
     pattern2 = re.compile("^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$") # email regex
-    if not (all(data[0:14]) and data[15] and data[17] and data[26] and data[25]): # existance check
+    if not (all(data[0:14]) and data[15] and data[17] and data[26] and data[25]): # existence check
         return False
     elif not all(map([data[11], data[13], data[15], data[17], data[26], data[25]], lambda x: (x == "Yes" or x == "No"))): # validate radio button
         return False
@@ -129,112 +130,114 @@ def validate_event(data):
     elif not pattern2.match(data[3]): # validate email
         return False
     else:
-        return True
+        return True # data passes all validation checks
         
-@app.route("/")
+@app.route("/") # default route
 def default():
-    return redirect(url_for('newevent'))
+    return redirect(url_for('newevent')) # redirect to new event page
 
-@app.route("/login")
+@app.route("/login") # login frontend route
 def login():
-    if auth.get(session.get('user')):
-        return redirect(url_for('manager'))
+    if auth.get(session.get('user')): # check user is logged in
+        return redirect(url_for('manager')) # redirect to manager page
     else:
-        return render_template('login.html', fail=request.args.get("fail"))
+        return render_template('login.html', fail=request.args.get("fail")) # login failed, display failure message
 
-@app.route("/loginpost", methods=['POST'])
+@app.route("/loginpost", methods=['POST']) # backend data handling for login
 def loginpost():
-    if request.form.get("username") and auth.get(request.form.get("username")) == request.form.get("password"):
+    if request.form.get("username") and auth.get(request.form.get("username")) == request.form.get("password"): # validate login (uses short circuiting logic)
         print("login success")
-        session['user'] = request.form.get("username")
-        return redirect(url_for('manager'))
+        session['user'] = request.form.get("username") # set session cookie in the browser to maintain login
+        return redirect(url_for('manager')) # redirect to manager page
     else:
-        print("login fail")
-        return redirect(url_for('login')+"?fail=true")
+        print("login fail") # validation failed
+        return redirect(url_for('login')+"?fail=true") # show failure message to user
         
-@app.route("/logout")
-@privileged
+@app.route("/logout") # logout route
+@privileged # privileged operation
 def logout():
-    session['user'] = None
-    return redirect(url_for('login'))
+    session['user'] = None # delete user session cookie
+    return redirect(url_for('login')) # redirect to login page
 
-@app.route("/manager")
-@privileged
+@app.route("/manager") # manager route
+@privileged # privileged operation
 def manager():
-    pending = [list(i) for i in cur.execute('SELECT * FROM events WHERE approval = "pending"').fetchall()]
-    approved = cur.execute('SELECT * FROM events WHERE approval = "approved"').fetchall()
-    rejected = cur.execute('SELECT * FROM events WHERE approval = "rejected"').fetchall()
-    for count, i in enumerate(pending):
+    pending = [list(i) for i in cur.execute('SELECT * FROM events WHERE approval = "pending"').fetchall()] # select pending events from the database
+    approved = cur.execute('SELECT * FROM events WHERE approval = "approved"').fetchall() # select approved events
+    rejected = cur.execute('SELECT * FROM events WHERE approval = "rejected"').fetchall() # select rejected events
+    for count, i in enumerate(pending): # loop the pending events
         print(i)
-        date = i[6]
-        conflict1 = calendar.get(date)
-        conflict2 = lunchtime_calendar.get(date)
-        pending[count].append(conflict1)
-        pending[count].append(conflict2)
+        date = i[6] # get the date
+        conflict1 = calendar.get(date) # find conflicts in the school calendar
+        conflict2 = lunchtime_calendar.get(date) # find conflicts in the lunchtime calendar
+        pending[count].append(conflict1) # add conflict to pending event
+        pending[count].append(conflict2) # add second conflict to pending event
             
-    return render_template('manager.html', pending=pending, approved=approved, rejected=rejected)
+    return render_template('manager.html', pending=pending, approved=approved, rejected=rejected) # render the manager gui with the calculated data
 
+# new event route
 @app.route("/newevent")
 def newevent():
     return render_template('newevent.html', 
                            fail=request.args.get("fail"),
                            schoolcalendar=schoolcalendar,
-                           activitycalendar=activitycalendar)
+                           activitycalendar=activitycalendar) # render new event passing url of school calendar, activity data, booolean for prior login failure for fail message
                            
-@app.route("/neweventpost", methods=['POST'])
+@app.route("/neweventpost", methods=['POST']) # backend data handling for new event
 def neweventpost():
-    data = handle_event(request)
-    if validate_event(data) == False:
-        redirect(url_for('newevent')+"?fail=true")
-    d_hash = hashlib.sha224(str(data).encode("utf-8")).hexdigest()
-    data.append(d_hash)
-    data.append("pending")
-    print(str(data))
-    cur.execute('INSERT INTO events VALUES({})'.format(('?,' * 30)[0:-1]), data)
-    return redirect(url_for('progress', eventhash=d_hash))
+    data = handle_event(request) # handle event data parsing
+    if validate_event(data) == False: # validate data
+        redirect(url_for('newevent')+"?fail=true") # redirect to fail page if validation fails
+    d_hash = hashlib.sha224(str(data).encode("utf-8")).hexdigest() # calculate sha224 hash
+    data.append(d_hash) # append hash to data in database
+    data.append("pending") # set new event as pending approval
+    print(str(data)) # print for debugging
+    cur.execute('INSERT INTO events VALUES({})'.format(('?,' * 30)[0:-1]), data) # insert data into database, autoformat db-api to prevent sql injection
+    return redirect(url_for('progress', eventhash=d_hash)) # redirect to progress page passing the hash
     
-@app.route("/progress/<eventhash>")
-def progress(eventhash):
-    approval = cur.execute('SELECT * FROM events WHERE eventhash = ?', (eventhash,)).fetchone()[29]
+@app.route("/progress/<eventhash>") # progress page with event hash
+def progress(eventhash): # accept event hash
+    approval = cur.execute('SELECT * FROM events WHERE eventhash = ?', (eventhash,)).fetchone()[29] # fetch one row of data from the database and index approval status
     print(eventhash)
-    teachers = [names.get(i) for i in auth if names.get(i) != None]
+    teachers = [names.get(i) for i in auth if names.get(i) != None] # get names of teachers
     print(teachers)
-    return render_template('progress.html', teachers=teachers, approval=approval)
+    return render_template('progress.html', teachers=teachers, approval=approval) # pass teachers and approval status (currently approval is simultaneous for all teachers)
     
-@app.route("/editevent/<eventhash>")
-@privileged
+@app.route("/editevent/<eventhash>") # event edit page for Ms. Nguyen to edit events
+@privileged # privileged user access only
 def editevent(eventhash):
-    event = cur.execute('SELECT * FROM events WHERE eventhash = ?', (eventhash,)).fetchone()
-    return render_template('editevent.html', fail=request.args.get("fail"),
+    event = cur.execute('SELECT * FROM events WHERE eventhash = ?', (eventhash,)).fetchone() # select event row from the database (event table)
+    return render_template('editevent.html', fail=request.args.get("fail"), # render page passing necessary data
                            schoolcalendar=schoolcalendar,
                            activitycalendar=activitycalendar,
-                           event=event, eventhash=eventhash)
+                           event=event, eventhash=eventhash) # passing event and event hash to fill the form fields
    
-@app.route("/editeventpost", methods=['POST'])
-@privileged
+@app.route("/editeventpost", methods=['POST']) # handle routing for event editing backend
+@privileged # privileged access
 def editeventpost():
-    data = handle_event(request)
-    if validate_event(data) == False:
-        return redirect(url_for('editevent', eventhash=data[28])+"?fail=true")
-    data.append(request.form.get(eventhash, "").trim())
-    data.append("pending")
-    with transaction(con):
-        cur.execute('DELETE FROM events WHERE eventhash = ?', (data[28],))
-        cur.execute('INSERT INTO events VALUES({})'.format(('?,' * 30)[0:-1]), data)
-    return redirect(url_for('manager'))
+    data = handle_event(request) # process event data
+    if validate_event(data) == False: # validate event data
+        return redirect(url_for('editevent', eventhash=data[28])+"?fail=true") # validation failed, redirect user
+    data.append(request.form.get(eventhash, "").trim()) # add formatted event hash to data
+    data.append("pending") # set event as pending
+    with transaction(con): # create a database transaction for ACID guarantees - ie. no database corruption even if there's power failure or OS crashes
+        cur.execute('DELETE FROM events WHERE eventhash = ?', (data[28],)) # delete previous data in the database
+        cur.execute('INSERT INTO events VALUES({})'.format(('?,' * 30)[0:-1]), data) # insert new data into the database
+    return redirect(url_for('manager')) # redirect to manager page
         
-@app.route("/approveevent/<eventhash>")
-@privileged
+@app.route("/approveevent/<eventhash>") # event approval handled with a GET request
+@privileged # privileged operation
 def approveevent(eventhash):
-    cur.execute('UPDATE events SET approval = "approved" WHERE eventhash = ?', (eventhash,))
-    return redirect(url_for('manager'))
+    cur.execute('UPDATE events SET approval = "approved" WHERE eventhash = ?', (eventhash,)) # update event event approval to approved
+    return redirect(url_for('manager')) # redirect back to manager page
     
-@app.route("/rejectevent/<eventhash>")
-@privileged
+@app.route("/rejectevent/<eventhash>") # event rejection route
+@privileged # privileged user 
 def rejectevent(eventhash):
-    cur.execute('UPDATE events SET approval = "rejected" WHERE eventhash = ?', (eventhash,))
-    return redirect(url_for('manager'))
-       
+    cur.execute('UPDATE events SET approval = "rejected" WHERE eventhash = ?', (eventhash,)) # update event approval to rejected
+    return redirect(url_for('manager')) # redirect back to manager page
+
+# account creation outside of project scope       
 #@app.route("/newaccount")
 #@privileged
 #def newaccount():
@@ -244,6 +247,7 @@ def rejectevent(eventhash):
 #@privileged
 #def newaccountpost():
 #    pass
-                           
+
+# check file is being run                           
 if __name__ == "__main__":
-    app.run()
+    app.run() # run app
